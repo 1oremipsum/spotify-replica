aebe53bc97f1e667fec45c2f797792cb8218dac2
Allan San <allanterzisanches@gmail.com>
2025-02-11 17:21:39 -0300

    Created the components (Header, Main) and configured base css & template of main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-// rafce
+import Header from "./components/Header"
+import Main from "./components/Main"
+
 const App = () => {
   return (
-    <h1>Ok</h1>
+    <>
+      <Header />
+      <Main/>
+    </>
   )
 }
 
