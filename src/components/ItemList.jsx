c2a9a9ebc620cf40ed00745df2f7b0f3a0545701
Allan San <allanterzisanches@gmail.com>
2025-02-14 17:20:50 -0300

    Creation of templates related to the specific visualization of a song and artist (album). Capturing objects and referencing them in the template.

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
-import SingleItem from './SingleItem';
-import { Link } from 'react-router-dom';
+import React from "react";
+import SingleItem from "./SingleItem";
+import { Link, useLocation } from "react-router-dom";
 
-const ItemList = ({title, items, itemsArray, path, idPath}) => {
-    return (
-        <div className="item-list">
-            <div className="item-list__header">
-                <h2>{ title } Populares</h2>
-                <Link to={path} className="item-list__link" >Mostrar Tudo</Link>
-            </div>
+const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+  const { pathname } = useLocation();
+  const isHome = pathname === "/";
+  const finalItems = isHome ? items : Infinity;
 
-            <div className="item-list__container">
-                {itemsArray.filter((currentValue, index) => index < items)
-                .map((currentObj, index) => (
-                    <SingleItem key={`${title}-${index}`}
-                        idPath={idPath}
-                        {...currentObj}
-                    />
-                ))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="item-list">
+      <div className="item-list__header">
+        <h2>{title} populares</h2>
+
+        {isHome ? (
+          <Link to={path} className="item-list__link">
+            Mostrar tudo
+          </Link>
+        ) : (
+          <></>
+        )}
+      </div>
+
+      <div className="item-list__container">
+        {itemsArray
+          .filter((currentValue, index) => index < finalItems)
+          .map((currObj, index) => (
+            <SingleItem {...currObj}
+              idPath={idPath}
+              key={`${title}-${index}`}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
 
 export default ItemList;
